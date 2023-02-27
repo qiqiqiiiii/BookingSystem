5376d0ed063bf739f3aebe3125e34f8f09734b9f
HHH <842527323@qq.com>
2023-02-27 14:57:38 +0800

    first commit

--- service/service_hosp/src/main/java/com/njupt/hosp/controller/HospitalController.java
+++ service/service_hosp/src/main/java/com/njupt/hosp/controller/HospitalController.java
@@ -2,12 +2,9 @@ package com.njupt.hosp.controller;
 
 import com.njupt.hosp.model.hosp.HospitalSet;
 import com.njupt.hosp.service.HospitalSetService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,13 @@ public class HospitalController {
     @ApiOperation(value = "查询医院设置表的所有信息",notes = "获取医院设置表中的所有信息")
     public List<HospitalSet> findAllHospitalSet(){
         return hospitalSetService.list();
+    }
+
+    //删除医院设置
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除医院设置",notes = "根据医院的Id来删除医院设置表中的信息")
+    public boolean removeHospitalSet(@PathVariable Long id){
+        return hospitalSetService.removeById(id);
     }
 
 
